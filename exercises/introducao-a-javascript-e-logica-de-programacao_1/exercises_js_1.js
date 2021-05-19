01d649e5738f247847667873a524033b8259d809
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-19 16:54:53 -0300

    entrega do exercises 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,11 +1,19 @@
 let numero1;
 let numero2;
+let numero3;
 numero1 = 15;
-numero2 = 25;
+numero2 = 16;
+numero3 = 10;
 
-if(numero1 > numero2){
-    console.log("numero1 maior que numero2")
+if(numero1 > numero2 && numero3 ){
+    console.log("numero1 maior ")
+}
+else if(numero2 > numero1 && numero3){
+    console.log( "numero2 maior ")
+}
+else if(numero3 > numero1 && numero2){
+    console.log("numero3 maior")
 }
 else{
-    console.log( "numero2 maior que numero1")
+    console.log("nao definido")
 }
