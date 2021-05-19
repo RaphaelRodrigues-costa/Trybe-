873fac9362728dc3f4a8ee10a950ac132f3f0e20
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-19 20:00:08 -0300

    entrega do execise 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,16 +1,15 @@
-//EX: 4
-let valor;
-valor = -10;
+let angulo1 = 100;
+let angulo2 = 40;
+let angulo3 = 40;
 
-if(valor > 0 ){
-    console.log("positive")
+let soma = angulo1 + angulo2 + angulo3;
+
+if(soma == 180 ){
+    console.log(true);
 }
-else if(valor < 0){
-    console.log("negative")
-}
-else if(valor === 0){
-    console.log("zero")
+else if(angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+  console.log("Erro");
 }
 else{
-    return;
+  console.log(false);
 }
