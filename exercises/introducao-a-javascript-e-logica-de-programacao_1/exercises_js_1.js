1f79812dcc4e649a63615a0b59c50e91806aaf4a
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-19 19:10:03 -0300

    entrega do execise 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,59 +1,16 @@
-// ex 1:
-let a;
-let b;
-a = 15
-b = 25
+//EX: 4
+let valor;
+valor = -10;
 
-let soma = a + b;
-let subtracao = a - b;
-let mutiplicacao = a * b;
-let divisao = a / b;
-let porcentagem = a % b;
-console.log(soma);
-console.log(subtracao);
-console.log(mutiplicacao);
-console.log(divisao);
-console.log(porcentagem);
-
-//ex 2:
-let numero1;
-let numero2;
-
-numero1 = 25;
-numero2 = 16;
-
-
-if(numero1 > numero2){
-    console.log("numero1 maior ")
+if(valor > 0 ){
+    console.log("positive")
 }
-else if(numero2 > numero1){
-    console.log( "numero2 maior ")
+else if(valor < 0){
+    console.log("negative")
+}
+else if(valor === 0){
+    console.log("zero")
 }
 else{
     return;
 }
-
-
-
-//ex 3:
-let numero1;
-let numero2;
-let numero3;
-numero1 = 25;
-numero2 = 16;
-numero3 = 50;
-
-if(numero1 > numero2 && numero1 > numero3 ){
-    console.log("numero1 maior ")
-}
-else if(numero2 > numero1 && numero2 > numero3){
-    console.log( "numero2 maior ")
-}
-else if(numero3 > numero1 && numero3 > numero2){
-    console.log("numero3 maior")
-}else{
-    return;
-}
-
-
-
