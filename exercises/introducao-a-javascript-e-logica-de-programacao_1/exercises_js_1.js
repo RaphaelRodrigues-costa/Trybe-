49c881034568e286a2715a16cdd4207246ab8e92
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-19 21:17:24 -0300

    entrega do execise 6

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,15 +1,29 @@
-let angulo1 = 100;
-let angulo2 = 40;
-let angulo3 = 40;
+let pecas = "Peao";
+let resultado = pecas.toLowerCase();
+switch (resultado) {
+  case "rei".toLowerCase():
+    console.log(resultado + ' Pode mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo.');
+      break;
 
-let soma = angulo1 + angulo2 + angulo3;
+  case "Dama".toLowerCase():
+    console.log(resultado + ' Assim como o Rei, pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres.');
+      break;
 
-if(soma == 180 ){
-    console.log(true);
-}
-else if(angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
-  console.log("Erro");
-}
-else{
-  console.log(false);
+    case "Torre".toLowerCase():
+    console.log(resultado + ' Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.');
+      break;
+
+  case "Bispo".toLowerCase():
+    console.log(resultado + ' Move-se na diagonal, quantas casas quiser. O Bispo que iniciar a partida em uma casa branca somente poderá transitar pelas brancas, enquanto o Bispo que inicia em uma casa preta somente poderá transitar pelas casas pretas.');
+      break;
+
+  case "Cavalo".toLowerCase():
+    console.log(resultado + ' É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”.');
+      break;
+
+  case "Peao".toLowerCase():
+    console.log(resultado + ' Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas. O peão é a única peça que não pode retroceder, e também a única que efetua a captura com um movimento diferente do utilizado para avançar no tabuleiro. ');
+      break;
+    
+  default : console.log("erro");
 }
