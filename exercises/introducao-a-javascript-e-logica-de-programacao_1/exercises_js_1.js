76930f2ab7cfa36b2ad0fedada2c83236ad7ef57
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-20 23:23:05 -0300

    entrega do execise 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,29 +1,26 @@
-let pecas = "Peao";
-let resultado = pecas.toLowerCase();
-switch (resultado) {
-  case "rei".toLowerCase():
-    console.log(resultado + ' Pode mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo.');
-      break;
 
-  case "Dama".toLowerCase():
-    console.log(resultado + ' Assim como o Rei, pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres.');
-      break;
 
-    case "Torre".toLowerCase():
-    console.log(resultado + ' Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.');
-      break;
+let nota;
+nota = 50;
 
-  case "Bispo".toLowerCase():
-    console.log(resultado + ' Move-se na diagonal, quantas casas quiser. O Bispo que iniciar a partida em uma casa branca somente poderá transitar pelas brancas, enquanto o Bispo que inicia em uma casa preta somente poderá transitar pelas casas pretas.');
-      break;
-
-  case "Cavalo".toLowerCase():
-    console.log(resultado + ' É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”.');
-      break;
-
-  case "Peao".toLowerCase():
-    console.log(resultado + ' Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas. O peão é a única peça que não pode retroceder, e também a única que efetua a captura com um movimento diferente do utilizado para avançar no tabuleiro. ');
-      break;
-    
-  default : console.log("erro");
+if (nota >= 90 &&  nota <= 100 ){
+  console.log("A");
+}
+else if (nota >= 80 && nota <= 89){
+  console.log("B")
+}
+else if(nota >= 70 && nota <= 79){
+  console.log("C")
+}
+else if(nota >= 60 && nota <= 69){
+  console.log("D")
+}
+else if(nota >= 50 && nota <= 59){
+  console.log("E")
+}
+else if(nota < 50 && nota > 0 ){
+  console.log("F")
+}
+else {
+  console.log("erro")
 }
