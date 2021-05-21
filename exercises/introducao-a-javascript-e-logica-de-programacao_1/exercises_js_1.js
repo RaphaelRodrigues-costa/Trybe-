0262b6925fdc4eb06be6248ebc2c3e6fa1b9bc9e
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-05-21 14:20:11 -0300

    acabou

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_js_1.js
@@ -1,14 +1,59 @@
-let number1;
-let number2;
-let number3;
 
-number1 = 1;
-number2 = 10;
-number3 = 10;
+/*ex: 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-if(number1 % 2 == 1 || number2 % 2 == 1  || number3 % 2 == 1 ){
-  console.log(true)
+for(let index = 0; index < numbers.length; index += 1){
+  let imprimindoValores = numbers[index];
+  console.log(imprimindoValores)
 }
-else{
-  console.log(false)
+ex 2
+
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    soma = soma + numbers[index] ;
+   
+    
 }
+console.log(soma)
+
+
+ex:3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    soma = soma + numbers[index] 
+    
+    }
+
+  let media = soma / numbers.length;
+  console.log(media)
+
+  ex:4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    soma = soma + numbers[index] 
+    
+    }
+
+  let media = soma / numbers.length;
+  
+  if (media >= 20){
+    console.log('valor maior que 20')
+  }else{
+    console.log('valor menor que 20')
+  }*/
+
+  
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let valor = 0;
+  for(let index = 0; index < numbers.length;index += 1){
+    valor = Math.max(valor)
+  }
+  console.log(valor)
